c940c7a2d1597d4e2be821c9efa182267348b895
vjguo <191892960@qq.com>
2022-03-30 23:06:34 +0800

    feat(global): add global styles

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
+import '@/styles/index.scss'
 import '@/plugins/qiankun.ts'
 
 createApp(App).use(router).mount('#app')
